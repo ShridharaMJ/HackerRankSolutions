4f86e242137e325c21178096f80c32d98704ce50
ShridharaMJ <shridharamj@gmail.com>
2021-11-16 11:44:02 +0530

    Committ Nov 16 1142AM

---
+++ HackerRanksSolutions/src/test/java/com/practice/arrays/RotateArrays.java
@@ -0,0 +1,52 @@
+package com.practice.arrays;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.testng.annotations.Test;
+
+public class RotateArrays {
+
+	@SuppressWarnings("removal")
+	@Test
+	public void testcase1() {
+
+		List<Integer> asList = Arrays.asList(1, 2, 3, 4, 5);
+		int numberOfRotate = 9;
+		
+
+		Integer[] array = asList.toArray(new Integer[asList.size()]);
+		int arrayLength = array.length;
+		if(numberOfRotate>arrayLength)
+			numberOfRotate=numberOfRotate%arrayLength;
+		
+		
+		
+		for (int i = 0; i < numberOfRotate; i++) {
+
+			// Integer[] tempArray = new Integer[array.length];
+			/*
+			 * for (int j = tempArray.length - 1; j >= 0; j--) {
+			 * 
+			 * if (j == tempArray.length - 1) { tempArray[j] = array[0]; // always take 1st
+			 * element to last else
+			 * 
+			 * } else tempArray[j] = array[j+1];
+			 * 
+			 * }
+			 */
+
+			int tempInt = array[0];
+			array = Arrays.copyOfRange(array, 1, arrayLength + 1);
+			array[arrayLength - 1] = tempInt;
+		}
+
+		for (Integer integer : array) {
+			System.out.println(integer);
+		}
+		// List<Integer> asList2 = Arrays.asList(array);
+
+	}
+
+}
